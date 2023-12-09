a258646f37267121990e7f57c8cdb1976372eb4c
Soham <mastpanchalsoham@gmail.com>
2023-12-09 07:06:13 +0530

    feat: Added chainID display to banner part of UI and Sponsor pop up for particular features

--- client/src/app/components/Banner.tsx
+++ client/src/app/components/Banner.tsx
@@ -1,8 +1,16 @@
+'use client'
+
 import React from 'react'
 
 import { Carousel, Image } from 'antd';
+import { useChainStore } from './store/chainStore';
 
 const Banner = () => {
+
+  const sponsorArr = ['1inch.png', 'safe.png', 'safe.png', 'push.png' ]
+
+  const {mode} = useChainStore() as any;
+
   return (
     
     <div className='flex flex-col h-full items-center justify-around w-2/5 bg-[#87C4FF78]'>
@@ -12,24 +20,21 @@ const Banner = () => {
           <div className='text-lg mt-5 text-black'>Your Preferred <span className='font-semibold'>Currency</span>, Your Seamless <span className='font-semibold'>Trade</span></div>
         </div>
 
-        <div className='flex flex-col h-1/2 items-center justify-center w-full'>
-          <div className='h-10% text-lg text-black'>Powered by</div>
-          <div className='h-70%'>
-          <Carousel autoplay>
-          
-             <Image src="/logos/push.png" width={10} alt="" />
-          
-          
-             {/* <img src="" alt="" />
-        
-          
-             <img src="" alt="" />
-        
-          
-             <img src="" alt="" /> */}
-          
-         </Carousel>
+        <div className='flex flex-col h-1/4 items-center justify-center w-full'>
+          <img src={`/logos/${''+sponsorArr[mode]}`} width={150} height={50} alt="" />
+        </div>
+
+        <div className='flex flex-col h-1/4 items-center justify-center w-full'>
+          <div className='flex flex-col items-center justify-evenly h-30% w-95% text-lg text-black'>
+            {/* User add */}
+            {/* Chain id */}
+            <div className='flex flex-row items-center h-1/6 w-30% text-lg font-semibold text-black'>Chain ID:</div>
+            <div className='flex flex-row items-center h-1/6 w-30%  text-black'>0x677953216891546</div>
+            {/* User name */}
+            {/* Email id */}
+            {/*  */}
           </div>
+          
         </div>
       </div>
   )
--- client/src/app/components/message/Message.tsx
+++ client/src/app/components/message/Message.tsx
@@ -2,7 +2,18 @@ import React from 'react'
 
 const Message = () => {
   return (
-    <div>Message</div>
+    <div className='w-full h-full flex flex-col items-center '>
+        <div className='flex items-center justify-center w-80% h-70% overflow-y-scroll'>
+            {/* Array(5).fill(0).map((_, i) => {return(<div className='flex flex-col items-start justify-start w-full h-20%'></div>w)}) */}
+        </div>
+
+        <div className='flex items-center justify-between w-80% h-25% rounded-[6px] bg-[#87C4FF78] px-4'> 
+            <input type="text" placeholder='Type a Message!' className='w-70% h-full p-1 px-3 rounded-[6px] bg-transparent' />
+            <div className='h-[32px] w-[32px] flex items-center justify-center cursor-pointer rounded-full hover:bg-[#5a84ae78] '>
+                <img src="/icons/send.png" alt="" />
+            </div>
+        </div>
+    </div>
   )
 }
 
